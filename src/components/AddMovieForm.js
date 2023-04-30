af6791d78b4264482063d6ca00b911dc9b143266
Sydney Hopson <sydneyhopsonreignmobile@Sydneys-iMac.local>
2023-04-30 08:53:52 -0400

    Finished all except edit friendslist stretch goal

--- src/components/AddMovieForm.js
+++ src/components/AddMovieForm.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import {  useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 import axios from 'axios';
 
-const EditMovieForm = (props) => {
+const AddMovieForm = (props) => {
   const navigate = useNavigate();
-  const { id } = useParams();
+  
   
 
-  const { setMovies } = props;
+  
   const [movie, setMovie] = useState({
     title: "",
     director: "",
@@ -24,24 +24,13 @@ const EditMovieForm = (props) => {
       [e.target.name]: e.target.value
     });
   }
-
-  useEffect(() =>{
- axios.get(`http://localhost:9000/api/movies/${id}`)
-    .then(res => {
-     setMovie(res.data)
-    })
-    .catch(err => {
-      console.log(err.response)
-    })
-  }, []);
-
-  const handleSubmit = (e) => {
+    const handleSubmit = (e) => {
     e.preventDefault();
-    axios.put(`http://localhost:9000/api/movies/${id}`, movie)
+    axios.post(`http://localhost:9000/api/movies`, movie)
       .then(res => {
         
         setMovie(res.data);
-        navigate(`/movies/${movie.id}`);
+        navigate(`/movies`);
       })
       .catch(err => {
         console.log(err);
@@ -55,7 +44,7 @@ const EditMovieForm = (props) => {
       <div className="modal-content">
         <form onSubmit={handleSubmit}>
           <div className="modal-header">
-            <h4 className="modal-title">Editing <strong>{movie.title}</strong></h4>
+            <h4 className="modal-title">Adding <strong>{movie.title}</strong></h4>
           </div>
           <div className="modal-body">
             <div className="form-group">
@@ -89,4 +78,4 @@ const EditMovieForm = (props) => {
     </div>);
 }
 
-export default EditMovieForm;
+export default AddMovieForm;
